7abef8866a8755f37b18491cb9c9d53d6af866e2
Arseny <44338288+parhimarsen@users.noreply.github.com>
2019-02-09 21:55:49 +0300

    Update FancyCalc.cs

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -11,27 +11,52 @@ namespace FancyCalc
 
         public double Add(int a, int b)
         {
-            throw new NotImplementedException();
-            //return a + b;
+            //throw new NotImplementedException();
+            return Convert.ToDouble(a + b);
         }
 
 
         public double Subtract(int a, int b)
         {
             //throw new NotImplementedException();
-            return a - b;
+            return Convert.ToDouble(a - b);
         }
 
 
         public double Multiply(int a, int b)
         {
-            return a * b;
+            return Convert.ToDouble(a * b);
         }
 
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
+            string[] words = expression.Split(' ');
+            switch (words[1])
+            {
+                case ("+"):
+                    return Convert.ToDouble(words[0]) + Convert.ToDouble(words[2]);
+                    break;
+                case ("-"):
+                    return Convert.ToDouble(words[0]) - Convert.ToDouble(words[2]);
+                    break;
+                case ("*"):
+                    return Convert.ToDouble(words[0]) * Convert.ToDouble(words[2]);
+                    break;
+                case ("/"):
+                    if (Convert.ToInt16(words[2]) != 0)
+                    {
+                        return Convert.ToDouble(words[0]) / Convert.ToDouble(words[2]);
+                    }
+                    else
+                    {
+                        throw new System.DivideByZeroException();
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    break;
+            }
 
         }
     }
